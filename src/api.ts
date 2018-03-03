fb69ef88bfde9c1e3df6e8e3ccf073dd215f1743
Tanner Goins <tannergz28@gmail.com>
2018-03-02 22:48:34 -0600

    simplify

--- src/api.ts
+++ src/api.ts
@@ -31,9 +31,8 @@ export class Api {
    */
   constructor () {
     this.app = express()
-    this.config()
     this.routes()
-    this.connect().catch((err: any) => Logger.error(err))
+    this.config()
   }
 
   /**
@@ -48,28 +47,6 @@ export class Api {
   }
 
   public config () {
-    this.app.use(express.static(path.join(__dirname, 'public')))
-
-    this.app.use(morgan('tiny', {
-      stream: {
-        write: message => Logger.info(message.trim())
-      }
-    } as morgan.Options))
-
-    this.app.use(bodyParser.json({
-      limit: '50mb'
-    }))
-
-    this.app.use(bodyParser.urlencoded({
-      extended: true
-    }))
-
-    this.app.use(helmet())
-    this.app.use(cors())
-    this.app.use(compression())
-    this.app.use(methodOverride())
-    this.app.use(expressStatusMonitor())
-
     this.app.use((err: any, req: Request, res: Response, next: NextFunction) => {
       err.status = 404
       next(err)
@@ -79,23 +56,19 @@ export class Api {
   }
 
   private routes () {
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-      (this.app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-        const result = (new (route.controller as any)())[route.action](req, res, next)
-        if (result instanceof Promise) {
-          result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
+    createConnection().then(async connection => {
+      // register express routes from defined application routes
+      Routes.forEach(route => {
+        (this.app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+          const result = (new (route.controller as any)())[route.action](req, res, next)
+          if (result instanceof Promise) {
+            result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
 
-        } else if (result !== null && result !== undefined) {
-          res.json(result)
-        }
+          } else if (result !== null && result !== undefined) {
+            res.json(result)
+          }
+        })
       })
-    })
-  }
-
-  private async connect () {
-    const connection = await createConnection()
-    this.connection = connection
-    return connection
+    }).catch((err) => Logger.error(err))
   }
 }
