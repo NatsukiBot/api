e69ccc43cf91dccc11da2328b805fa9ffb87c2b3
Jason Bothell <35819370+jasonhaxstuff@users.noreply.github.com>
2018-03-11 19:54:28 -0400

    Create guild.ts

---
+++ src/services/guild.ts
@@ -0,0 +1,15 @@
+import { User, UserLevel } from '@natsuki/db'
+import { getRepository, getConnection } from 'typeorm'
+import { provide } from '../ioc/ioc'
+import { TYPES } from '../constants'
+import { Logger } from '../utility'
+
+/**
+ * Guild service that handles storing and modifying guild data
+ *
+ * @class GuildService
+ */
+@provide(TYPES.GuildService)
+export class GuildService {
+  // TODO: Replace with Guild models
+}
