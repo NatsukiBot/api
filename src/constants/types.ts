4734bcec59c26237b95641be95398c5dd7021bee
Jason Bothell <35819370+jasonhaxstuff@users.noreply.github.com>
2018-03-11 20:02:03 -0400

    Update types.ts

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,6 +1,7 @@
 export const TYPES = {
   UserService: Symbol('UserService'),
   GuildService: Symbol('GuildService'),
+  GuildController: Symbol('GuildController'),
   DatabaseProvider: Symbol('DatabaseProvider'),
   ConfigProvider: Symbol('ConfigProvider')
 }
