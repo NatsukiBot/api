16e70d5953c77c391391e84ada790abd2255873d
Jason Bothell <35819370+jasonhaxstuff@users.noreply.github.com>
2018-03-11 20:31:15 -0400

    Update guild.ts

--- src/controller/guild.ts
+++ src/controller/guild.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { controller, httpGet, httpDelete, httpPut, httpPost } from 'inversify-express-utils'
 import { inject } from 'inversify'
 import { TYPES } from '../constants'
-import { UserService } from '../services/user'
+import { GuildService } from '../services/guild'
 
 /**
  * The Guild controller. Contains all endpoints for handling Guilds and Guild data.
@@ -12,6 +12,6 @@ import { UserService } from '../services/user'
  */
 @controller('/api/guild')
 export class GuildController {
-  constructor (@inject(TYPES.GuildService) private GuildService: GuildService) {}
+  constructor (@inject(TYPES.GuildService) private guildService: GuildService) {}
   // TODO: Replace with Guild models
 }
